68ee57bb6c15de78c057aa6efb9df100c37f76ce
shubhampatwa <shubhampatwa526@gmail.com>
2022-02-01 14:05:00 +0530

    Added env variables

--- apps/server/src/environments/environment.prod.ts
+++ apps/server/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  appEnv: process.env.NODE_ENV,
+  port: 3000,
 };
--- apps/server/src/environments/environment.ts
+++ apps/server/src/environments/environment.ts
@@ -1,3 +1,6 @@
+// this file would be replaced by the .prod sibling during build
 export const environment = {
   production: false,
+  appEnv: process.env.NODE_ENV,
+  port: 3000,
 };
