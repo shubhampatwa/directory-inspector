aadcf3824c0f8b0213daa58718c4d8d83caedc2f
shubhampatwa <shubhampatwa526@gmail.com>
2022-02-01 15:50:00 +0530

    Extending the node-cache module

---
+++ libs/local-cache/src/lib/constants.ts
@@ -0,0 +1,2 @@
+
+export const TTL = 60 * 60;
--- libs/local-cache/src/lib/local-cache.ts
+++ libs/local-cache/src/lib/local-cache.ts
@@ -1,3 +1,11 @@
-export function localCache(): string {
-  return 'local-cache';
+import * as NodeCache from 'node-cache';
+import { TTL } from './constants';
+
+export class LocalCache extends NodeCache {
+  constructor(options: NodeCache.Options) {
+    super({
+      stdTTL: TTL,
+      ...options
+    })
+  }
 }
