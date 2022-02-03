959a6e556184f3c52e76d40d0e6ef79861680005
shubhampatwa <shubhampatwa526@gmail.com>
2022-02-03 23:08:50 +0530

    Style the load more files button

--- libs/file-explorer/src/lib/components/NodeLabel.tsx
+++ libs/file-explorer/src/lib/components/NodeLabel.tsx
@@ -1,12 +1,20 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { MORE_FILES_NODE_NAME } from '../constants';
 import { NodeLabelProps, FileNode } from '../types'
 
-const prepareNodeLabel = (node: FileNode, parent?: FileNode) => node.path.replace(`${parent?.path}/` || '', '')
+const prepareNodeLabel = (node: FileNode, parent?: FileNode) =>
+  node.path.replace(`${parent?.path}/` || '', '')
 
 const NodeLabel = (props: NodeLabelProps) => (
   <Box display='flex'>
-    <Box flexGrow={1} component={Typography}>{prepareNodeLabel(props.node, props.parent)}</Box>
+    <Box
+      flexGrow={1}
+      component={Typography}
+      color={props.node.fileName === MORE_FILES_NODE_NAME ? 'mediumblue' : 'black'}
+    >
+      {prepareNodeLabel(props.node, props.parent)}
+    </Box>
     {!!props.node.size && <Box mr={1} component={Typography}>{props.node.size}</Box>}
   </Box>
 );
